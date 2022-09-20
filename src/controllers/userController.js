ea670ce70a008a1baccfc773a0871874b53099e7
vikramsingh7568 <110588921+vikramsingh7568@users.noreply.github.com>
2022-09-20 15:54:59 +0530

    done

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,5 @@
 const userModel = require("../models/userModel.js")
-
+const jwt = require('jsonwebtoken')
 
 //--------------------------|| CREATE USERS ||--------------------------------
 
@@ -18,6 +18,48 @@ const createUser = async function(req,res){
 //--------------------------|| LOGIN USERS ||--------------------------------
 
 const loginUser = async function(req,res){
+    try{
+
+     let {email , password} = req.body
+
+     if (!email) {
+        return res.status(400).send({
+             status: false, message: "EmailId is mandatory"
+        })
+   }
+
+   // Password is mandatory
+
+   if (!password) {
+        return res.status(400).send({
+             status: false, message: "Password is mandatory"
+        })
+   }
+
+     
+     let DataChecking = await userModel.findOne({email :email, password:password})
+     if(DataChecking.length==0){
+        return res.status(404).send({msg : "Please enter valid email or password"})
+     }
+     
+     let token = jwt.sign(
+        {
+             userId: DataChecking._id.toString(),
+             batch: "Plutonium",
+             organisation: "Project-3, Group-31"
+        },
+        "Book-Management",{
+
+          expiresIn: '1M' // expires in 1m hours
+
+           });
+   return res.status(201).send({ status: true, message: token })
+}
+catch (error) {
+   res.status(500).send({
+        status: false, message: error.message
+   })
+}
 
 }
 
