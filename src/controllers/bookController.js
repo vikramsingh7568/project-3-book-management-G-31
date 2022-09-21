be047e5b576abf7dc495ebef540c3d97eff96ca4
Mani03011998 <mani.ratna.monumamta@gmail.com>
2022-09-21 15:13:26 +0530

    GetbyParams

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,6 +1,8 @@
 
 const bookModel = require("../models/bookModel.js")
 const userModel = require("../models/userModel.js")
+const reviewModel = require("../models/reviewModel")
+
 const mongoose = require("mongoose");
 
 
@@ -119,8 +121,59 @@ const getBooks = async function(req,res){
 //--------------------------|| GET BOOKS BY PARAMS ||--------------------------------
 
 const getBookByparam = async function(req,res){
+    try{
+        let bookId = req.params.bookId;
+        if (!bookId) {
+            return res.status(400).send({ status: false, message: "please provide a bookId in params" })
+            
+        };
+        if(!isValidObjectId(bookId)){
+           return res.status(400).send({status: false, msg: `${bookId} is not valid book Id`})
+        }
+        let findbookId = await bookModel.findById(bookId)
+        
+        if(!findbookId) {
+        return res.send({status: false, msg: "book Id is not valid"})    
+        }
+        let bookData = await bookModel.findOne({
+            _id:bookId
+        })
+        
+        let reviewFind = await reviewModel.find({
+            bookId:bookId
+        })
+        let bookData2 ={
+            data : bookData,
+            reviewsData : reviewFind
+        }
+        let  Data = {
+            name : specificData.name,
+            fullName : specificData.fullName,
+            logoLink : specificData.logoLink,
+            interns  : specificData2
+          }
+    
+        
+        return res.status(200).send({status:true,message: 'Books list',data : bookData2 })
+
+    }catch(err){
+       return res.status(500).send(err.message)
+    }
     
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //--------------------------|| UPDATE BOOKS ||--------------------------------
 
